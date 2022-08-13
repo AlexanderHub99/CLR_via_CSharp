4e0745bb6de279a93a677c8148abf7f16145ae90
Александр Середин <agames1448@gmail.com>
2022-08-13 19:05:16 +0300

    [TypeDynamic] Пример работы и упрощения с помощью  типа данных Dynamic

--- CLR_via_CSharp/Program.cs
+++ CLR_via_CSharp/Program.cs
@@ -1,4 +1,8 @@
 using System;
-
+using CLR_via_CSharp.PrimitiveDataType;
 
 Console.WriteLine("Hello, World!");
+TypeDynamicTest typeDynamicTest = new TypeDynamicTest();
+
+Console.WriteLine(typeDynamicTest.Result());
+Console.WriteLine(typeDynamicTest.Result2());
